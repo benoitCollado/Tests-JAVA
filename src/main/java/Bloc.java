192249aba36c3a049f333217c57b8f9634052a0b
benoit_collado <40575928+benoitCollado@users.noreply.github.com>
2024-11-28 11:35:18 +0000

    suppression des éléments inutiles

--- src/main/java/Bloc.java
+++ src/main/java/Bloc.java
@@ -1,11 +1,8 @@
-/*import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-*/
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 public abstract class Bloc implements IBloc{
 
-/*  private static final Logger logger = LogManager.getLogger(Bloc.class);*/
   protected static final Logger logger = Logger.getLogger(Bloc.class.getName());
   protected float longueur;
   protected float largeur;
@@ -14,15 +11,14 @@ public abstract class Bloc implements IBloc{
 
   Bloc(final float longueur, final float largeur, final float hauteur) throws IllegalBlocException {
     if(longueur < MIN_LONGUEUR || largeur < MIN_LARGEUR || hauteur < MIN_HAUTEUR) {
-      /*logger.error("Les valeurs minimales pour longeur, larger et hauteur n'ont pas été respectées");*/
+
       logger.log(Level.SEVERE, "Les valeurs minimales pour longeur, larger et hauteur ne sont pas respectées");
       throw new IllegalBlocException();
     }
     this.longueur = longueur;
     this.largeur = largeur;
     this.hauteur = hauteur;
-    logger.log(Level.INFO, "Un Bloc de type : " + this.getClass().getName() + " a été créé");
-    /*logger.info("Un bloc de type : " + this.getClass().getName() + " a été créé" );  */  
+    logger.log(Level.INFO, "Un Bloc de type : " + this.getClass().getName() + " a été créé"); 
   }
 
   public float getLongueur(){
